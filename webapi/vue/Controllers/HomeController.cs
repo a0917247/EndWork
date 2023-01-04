4ab556e46d911e84bb741f008d4a226d3f2c4a46
a0917247 <a0917247@gmail.com>
2023-01-04 20:55:10 +0800

    html 放進 MVC 成功運行

---
+++ webapi/vue/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using vue.Models;
+
+namespace vue.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult source()
+        {
+            return View();
+        }
+
+        public IActionResult memberLogin()
+        {
+            return View();
+        }
+        public IActionResult platform()
+        {
+            return View();
+        }
+
+        public IActionResult jobList()
+        {
+            return View();
+        }
+
+
+        public IActionResult courseManage()
+        {
+            return View();
+        }
+
+        public IActionResult jobSingle()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
