72400cb2adfa21838a429ffb9f1bc60198d88aa0
林木森森 <zxcasd1545414453@gmail.com>
2023-01-05 16:29:29 +0800

    轉成MVC

--- webapi/vue/Controllers/HomeController.cs
+++ webapi/vue/Controllers/HomeController.cs
@@ -18,35 +18,46 @@ namespace vue.Controllers
             return View();
         }
 
+        // 資源
         public IActionResult source()
         {
             return View();
         }
 
+        
+        //會員中心
         public IActionResult memberLogin()
         {
             return View();
         }
+
+        public IActionResult memberSignup()
+        {
+            return View();
+        }
+
+        //論壇
         public IActionResult platform()
         {
             return View();
         }
 
+        //找工作
         public IActionResult jobList()
         {
             return View();
         }
+        public IActionResult jobSingle()
+        {
+            return View();
+        }
 
-
+        //課程
         public IActionResult courseManage()
         {
             return View();
         }
 
-        public IActionResult jobSingle()
-        {
-            return View();
-        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
