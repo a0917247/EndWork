eecdfe9edfb10f2c919940116da1a44942b028d2
jing <a0917247@gmail.com>
2023-01-06 22:29:45 +0800

    修復進入雲端資料庫問題

--- webapi/vue/wwwroot/js/custom.js
+++ webapi/vue/wwwroot/js/custom.js
@@ -220,6 +220,23 @@ jQuery(function ($) {
             nav: false,
             smartSpeed: 1000,
         });
+
+        $(".owl-carousel").owlCarousel({
+            loop: true,
+            margin: 10,
+            nav: true,
+            responsive: {
+                0: {
+                    items: 1,
+                },
+                600: {
+                    items: 3,
+                },
+                1000: {
+                    items: 5,
+                },
+            },
+        });
     };
     owlCarouselFunction();
 
