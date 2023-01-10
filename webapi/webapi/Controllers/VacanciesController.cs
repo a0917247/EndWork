ab0d0bcae0713f42b231fa9e3803bb3634216b64
jing <a0917247@gmail.com>
2023-01-10 23:30:20 +0800

    完成備份

--- webapi/webapi/Controllers/VacanciesController.cs
+++ webapi/webapi/Controllers/VacanciesController.cs
@@ -12,7 +12,7 @@ using static System.Net.WebRequestMethods;
 
 namespace webapi.Controllers
 {
-    [EnableCors("AllowAny")]
+    [EnableCors("AllowAny")] 
     [Route("api/[controller]")]
     [ApiController]
     public class VacanciesController : ControllerBase
@@ -26,7 +26,7 @@ namespace webapi.Controllers
 
         // GET: api/Vacancies
         [HttpGet]
-        public async Task<IEnumerable<VacancyDTO>> Get()
+        public async Task<IEnumerable<VacancyDTO>> Get(string? name, string? category, string? adress)
         {
             var result = _context.Vacancy.Join(_context.Enterprise, x => x.EnterpriseId, y => y.EnterpriseId, (van, etp) => new VacancyDTO
             {
@@ -46,6 +46,19 @@ namespace webapi.Controllers
                 UniformNumbers = etp.UniformNumbers
 
             });
+            if (!string.IsNullOrWhiteSpace(name))
+            {
+                result = result.Where(a => a.WorkName.Contains(name) || a.CompanyName.Contains(name));
+            }
+            if (!string.IsNullOrWhiteSpace(category)) 
+            {
+                result = result.Where(a => a.Category.Contains(category));
+            }
+            if (!string.IsNullOrWhiteSpace(adress)) 
+            {
+                result = result.Where(a => a.Address.Contains(adress));
+            }
+
             return await Task.FromResult(result);
 
             /*var result = from van in _context.Vacancy
@@ -67,33 +80,33 @@ namespace webapi.Controllers
         } //完成
 
         // GET: api/Vacancies/5
-        [HttpGet("{name}")]
-        public async Task<IEnumerable<VacancyDTO>> Getsearch(string name)
-        {
-            var result = _context.Vacancy.Join(_context.Enterprise, x => x.VacancyId, y => y.EnterpriseId, (van, etp) => new VacancyDTO
-            {
-                WorkName = van.WorkName,
-                WorkPlace = van.WorkPlace,
-                Salary = van.Salary,
-                FullPartTime = van.FullPartTime,
-                Shift = van.Shift,
-                WorkContent = van.WorkContent,
-                updatetime = van.Updatetime,
-                Seniority = van.Seniority,
-                Category = van.Category,
-                CompanyName = etp.CompanyName,
-                Address = etp.Address,
-                Info = etp.Info,
-                img = etp.Img,
-                UniformNumbers = etp.UniformNumbers
+        //[HttpGet("{name}")]
+        //public async Task<IEnumerable<VacancyDTO>> Getsearch(string name)
+        //{
+        //    var result = _context.Vacancy.Join(_context.Enterprise, x => x.VacancyId, y => y.EnterpriseId, (van, etp) => new VacancyDTO
+        //    {
+        //        WorkName = van.WorkName,
+        //        WorkPlace = van.WorkPlace,
+        //        Salary = van.Salary,
+        //        FullPartTime = van.FullPartTime,
+        //        Shift = van.Shift,
+        //        WorkContent = van.WorkContent,
+        //        updatetime = van.Updatetime,
+        //        Seniority = van.Seniority,
+        //        Category = van.Category,
+        //        CompanyName = etp.CompanyName,
+        //        Address = etp.Address,
+        //        Info = etp.Info,
+        //        img = etp.Img,
+        //        UniformNumbers = etp.UniformNumbers
 
-            });
-            if (!string.IsNullOrWhiteSpace(name)) 
-            {
-                result = result.Where(a => a.WorkName.Contains(name) || a.CompanyName.Contains(name));
-            }
-            return result;
-        } //完成
+        //    });
+        //    if (!string.IsNullOrWhiteSpace(name)) 
+        //    {
+        //        result = result.Where(a => a.WorkName.Contains(name) || a.CompanyName.Contains(name));
+        //    }
+        //    return result;
+        //} //完成
 
         // PUT: api/Vacancies/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
